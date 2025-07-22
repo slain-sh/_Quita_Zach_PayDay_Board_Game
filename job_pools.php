f1f075baa8f9ce859dc59b6205faaf19c74a5a41
riribuns <ngricoh@gmail.com>
2025-07-22 19:12:17 +0800

    job pools update, tell me whatchu think

--- job_pools.php
+++ job_pools.php
@@ -1,14 +1,19 @@
 $college_jobs = [
-    ['title' => 'Software Engineer', 'salary' => 80000],
-    ['title' => 'Doctor', 'salary' => 100000],
-    ['title' => 'Architect', 'salary' => 75000],
-    ['title' => 'Lawyer', 'salary' => 90000],
+    ['title' => 'OFW', 'salary' => 10000],
+    ['title' => 'St. Looks Doctor', 'salary' => 12000],
+    ['title' => 'ADS-CVN Star', 'salary' => 11000],
+    ['title' => 'ZIIT College Proffesor', 'salary' => 9500],
+    ['title' => 'TukTok Influencer w/ Masters Degree', 'salary' => 9000],
+    ['title' => 'ChetGPT Ethics Officer', 'salary' => 11500],
+    ['title' => 'UniQmo Fashion Designer', 'salary' => 10500],
 ];
 
 $career_jobs = [
-    ['title' => 'Cashier', 'salary' => 5000],
-    ['title' => 'Jollibugs Employee', 'salary' => 6500],
-    ['title' => 'Delivery Rider', 'salary' => 6000],
-    ['title' => 'Factory Worker', 'salary' => 7000],
-    ['title' => 'Call Center Agent', 'salary' => 5500],
+    ['title' => 'PureBronze Cashier', 'salary' => 5000],
+    ['title' => 'Jollibogs Employee', 'salary' => 6500],
+    ['title' => 'GrubFud Delivery Rider', 'salary' => 6000],
+    ['title' => 'Timu Factory Worker', 'salary' => 7000],
+    ['title' => 'Vape Shop Owner', 'salary' => 5000],
+    ['title' => 'Stirbox Barista', 'salary' => 5500],
+    ['title' => 'TukTok Influencer', 'salary' => 6000]
 ];
