1aa09cde2413145d78d72afd56e536002b5d04ea
riribuns <ngricoh@gmail.com>
2025-07-22 18:07:59 +0800

    finished events and college events

--- board.php
+++ board.php
@@ -26,7 +26,10 @@ $events = [
     "Reminder for next time... You used \"Pay Later\" on Lazaddy and forgot about it. Pay ₱2,500" => -2500,
     "Ugh! Your dog chewed your earphones again, time for some new ones. Pay ₱1,000" => -1000,
     "Kuya, bayad mo 'yan. You spilled coffee on someone's laptop. Pay ₱5,000" => -5000,
-    "You flipped thrifted clothes and sold them online, ukay-ukay master! Claim ₱4,500" => 4500,
+    "But they're cute tho! You tipped the barista too much kasi cute. Pay ₱500" => -500,
+    "Oh no! Accidentally tapped Boop card 5 times on the NRT. Pay ₱500" => -500,
+    "Defeat. You were challenged to eat some unli-wings, but your stomach lost. Pay ₱2,500" => -2500,
+    "Woah! You flipped thrifted clothes and sold them online, ukay-ukay master! Claim ₱4,500" => 4500,
     "Tita in Canada randomly sent you padala! Claim ₱2,000" => 2000,
     "You found money on the street! Claim ₱1,000" => 1000,
     "Your investment paid off! Claim ₱4,000" => 4000,
@@ -34,17 +37,25 @@ $events = [
     "You helped the old lady cross the street! Claim ₱1,000" => 1000,
     "Your Shofee livestream comment got picked for a giveaway! Claim ₱2,000" => 2000,
     "Whenever I see girls and boys~ You started carolling to the neighbors! Claim ₱1,000" => 1000,
+    "Pambansang Alaga! Your dog won 'Cutest Pet' in the barangay pet day! Claim ₱2,000" => 2000,
+    "I'll take it! You won an e-raffle for load convertible to CCash! Claim ₱1,500" => 1500,
+    "Quick Cash! You collected and sold old newspapers and bottles to the junk shop. Claim ₱500" => 500,
+    "Hardcore Stan! You sold your extra BuTaS album to a fan! Claim ₱2,000" => 2000,
+    "Gaming is life! You joined a \"Mobile Leggings:Beng Beng\" tournament and got 2nd place! Claim ₱3,000" => 3000,
+    "You helped your grandma fix her WiFi twice! Claim ₱2,500 and a granny smooch" => 2500,
+    "For the money! You won a bet of calling your ex to tell them you still love them. Claim ₱2,000" => 2000
 ]
 $college_events = [
     "You got sick! Medicine's getting expensive these days. Pay ₱3,000" => -3000,
     "McDollibee midnight snack feels! Got hungry for some chicken nuggets and sundae. Pay ₱500" => -500,
     "Got scammed! Free Data 200 text scam, even gave your birthday and mother's maiden name. Pay ₱2,000" => -2000,
+    "Gastos lang ambag! You contributed money for materials AND snacks for your group project. Pay ₱1,000" => -1000,
+    "Instant regret... You joined 5 orgs. Paid all the fees. Regretted everything. Pay ₱1,500" => -1500,
     "You sold some of your homemade crinkles to your classmates! Claim ₱1,000" => 1000,
-    "You found money on the street! Claim ₱1,000" => 1000,
+    "Leaked Math quiz answers reached the whole section! Claim ₱3,000" => 3000,
     "Birthday gift! Claim ₱2,000" => 2000,
     "You helped the old lady cross the street! Claim ₱1,000" => 1000,
-    "Tita in Canada randomly sent you padala! Claim ₱2000" => 2000,
-    
+    "Basta 'di ako bumagsak! You offered to do your bestie's thesis formatting. Claim ₱2,000" => 2000
 ];
 
 // Process move
