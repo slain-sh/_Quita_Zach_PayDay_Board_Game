83b10042dee772a3b447128c66335279a42d02ab
Zach Quita <zachquita@gmail.com>
2025-07-22 19:19:00 +0800

    Fix spelling

--- job_pools.php
+++ job_pools.php
@@ -3,7 +3,7 @@ $college_jobs = [
     ['title' => 'OFW', 'salary' => 10000],
     ['title' => 'St. Looks Doctor', 'salary' => 12000],
     ['title' => 'ADS-CVN Star', 'salary' => 11000],
-    ['title' => 'ZIIT College Proffesor', 'salary' => 9500],
+    ['title' => 'ZIIT College Professor', 'salary' => 9500],
     ['title' => 'TukTok Influencer w/ Masters Degree', 'salary' => 9000],
     ['title' => 'ChetGPT Ethics Officer', 'salary' => 11500],
     ['title' => 'UniQmo Fashion Designer', 'salary' => 10500],
