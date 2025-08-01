885adfffc0a5719099019b1b296e7507f36d7166
treeform-iii <anoob7743@gmail.com>
2025-08-01 08:22:33 +0800

    Renamed db connection

--- DBconn.php
+++ DBconn.php
@@ -1,5 +1,5 @@
 <?php
-function get_db_connection() {
+function DBconnection() {
     $conn = new mysqli("localhost", "root", "", "payday_game"); 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
